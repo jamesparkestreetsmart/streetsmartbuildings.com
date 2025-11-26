d3043b278c460d39ce8cba03d5ab330ca0c7b96e
Tool Room Design <toolroomdesign@PrecisionStamping.local>
2025-11-26 15:14:56 -0600

    get individual site back up

--- app/sites/[siteid]/page.tsx
+++ app/sites/[siteid]/page.tsx
@@ -1,14 +1,12 @@
 // app/sites/[siteid]/page.tsx
 
-export default function SitePage({
-  params,
-}: {
-  params: { siteid: string };
-}) {
+export default async function SitePage(props: { params: Promise<{ siteid: string }> }) {
+  const { siteid } = await props.params;
+
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Site detail page</h1>
-      <p>Dynamic site ID: <code>{params.siteid}</code></p>
+      <p>Dynamic site ID: <code>{siteid}</code></p>
     </div>
   );
 }
