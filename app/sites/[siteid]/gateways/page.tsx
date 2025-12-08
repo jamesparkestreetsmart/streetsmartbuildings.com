6b25a4583f7040d00b86a933e024bf7935072428
Tool Room Design <toolroomdesign@PrecisionStamping.local>
2025-12-07 20:44:46 -0600

    Fix HA entity-sync endpoint and clean up PostCSS config

--- app/sites/[siteid]/gateways/page.tsx
+++ app/sites/[siteid]/gateways/page.tsx
@@ -1,6 +1,7 @@
 // app/sites/[siteid]/gateways/page.tsx
 import GatewayClientPage from "./GatewayClientPage";
 
+export const dynamic = "force-dynamic";
 export default async function Page(props: any) {
   const { siteid } = await props.params;
   return <GatewayClientPage siteid={siteid} />;
